655022f0a1eb71e7c40f1873870b91843b1495ca
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-03-28 04:26:28 +0900

    fix resize

--- src/resize.py
+++ src/resize.py
@@ -3,6 +3,7 @@
 import cv2
 import os
 import glob
+import tqdm
 
 
 def read_images(dirname):
@@ -12,8 +13,9 @@ def read_images(dirname):
     dirname: name of the directory.
     """
     pathr = os.path.join(dirname, "**")
-    paths = glob.iglob(pathr, recursive=True)
-    for p in paths:
+    paths = glob.glob(pathr, recursive=True)
+    paths = [p for p in paths if os.path.isfile(p)]
+    for p in tqdm.tqdm(paths):
         img = cv2.imread(p)
         if img is None:
             continue
@@ -26,23 +28,29 @@ def save_image(path, img):
 
 
 def main():
-    dirname = input(
-        "対象のフォルダ名を相対パスで指定してください。\n例 ./data/raw\n: "
-    )
-    if not dirname:
-        raise ValueError("対象のフォルダ名を指定してください。")
+    while True:
+        dirname = input("対象のフォルダ名を相対パスで指定してください。\n例 ./data/raw\n: ")
+        if dirname:
+            break
+        print("対象のフォルダ名は必ず指定してください。")
     dirname = dirname.rstrip("/").rstrip("\\")
     after_dirname = input(
-        f"保存先のフォルダ名を先程と同じ形式で入力してください。\n例 ./data/processed（デフォルト {dirname}_resized)\n: "
+        f"保存先のフォルダ名を先程と同じ形式で入力してください。\n例 ./data/processed（省略可。デフォルト {dirname}_resized)\n: "
     )
     if not after_dirname:
         after_dirname = dirname + "_resized"
     after_dirname = after_dirname.rstrip("/").rstrip("\\")
-    rate = input("リサイズの比率を小数で入力してください。(デフォルト 0.5): ")
-    if not rate:
-        rate = 0.5
-    else:
-        rate = float(rate)
+    while True:
+        rate = input("リサイズの比率を小数で入力してください。(省略可。デフォルト 0.5): ")
+        if not rate:
+            rate = 0.5
+            break
+        else:
+            try:
+                rate = float(rate)
+                break
+            except:
+                print("比率は小数で入力してください。")
 
     for p, img in read_images(dirname):
         resized = cv2.resize(img, None, fx=rate, fy=rate, interpolation=cv2.INTER_AREA)
