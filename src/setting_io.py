57278c60dd231ad9acf3217ee0751b3dacaf5307
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-05-12 15:28:50 +0900

    write setting_io

--- src/setting_io.py
+++ src/setting_io.py
@@ -1,21 +1,26 @@
-import csv
+import pandas as pd
 from collections import defaultdict
 
 
-def read_adjust_setting(filepath):
+def read_metadata(filepath: str):
+    with open(filepath, "r") as f:
+        csvdata = csv.reader(f)
+
+    metadata = {}
+    if len(csvdata) >= 3:
 
 
 
-def read_mark_setting(filepath, scale: float = None) -> dict:
+def read_mark_setting(filepath: str, scale: float = None) -> dict:
     with open(filepath, "r") as f:
         csvdata = csv.reader(f)
 
     marks = defaultdict(dict)
-    if len(csvdata) >= 1:
+    if len(csvdata) >= 3:
         for row in csvdata[1:]:
             category, value, x, y, r = row
             x, y, r = int(x), int(y), int(r)
             if scale:
-                x, y, r = x // scale, y // scale, r // scale
+                x, y, r = int(x * scale), int(y * scale), int(r * scale)
             marks[category][value] = (x, y, r)
-    return marks
+    return dict(marks)
