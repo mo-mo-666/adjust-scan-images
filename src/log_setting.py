2bd32b8c6a4b9003d5cbaee64fab931c86a4cf9d
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-06-22 03:27:29 +0900

    add log setting

---
+++ src/log_setting.py
@@ -0,0 +1,17 @@
+from logging import getLogger, StreamHandler, FileHandler
+from  typing import Union
+
+
+def get_logger(console_mode: Union[str, int] = "warning", logpath: Union[str, None]=None, file_mode: Union[str,int]="waring"):
+
+    logger = getLogger("stchger")
+    logger.setLevel(console_mode)
+    stream_handler = StreamHandler()
+    stream_handler.setLevel(console_mode)
+    logger.addHandler(stream_handler)
+    if logpath:
+        file_handler = FileHandler(logpath)
+        file_handler.setLevel(file_mode)
+        logger.addHandler(file_handler)
+
+    return logger
